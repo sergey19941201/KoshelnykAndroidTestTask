1f2172fc9df3f9e2a9404b325db2790a029200b3
sergey19941201 <koshelnick1994@mail.ru>
2017-03-31 23:37:36 +0300

    df

--- KoshelnykAndroidTestTask/Resources/Resource.Designer.cs
+++ KoshelnykAndroidTestTask/Resources/Resource.Designer.cs
@@ -31,12 +31,46 @@ namespace KoshelnykAndroidTestTask
 		public partial class Attribute
 		{
 			
+			// aapt resource value: 0x7f010000
+			public const int layoutManager = 2130771968;
+			
+			// aapt resource value: 0x7f010002
+			public const int reverseLayout = 2130771970;
+			
+			// aapt resource value: 0x7f010001
+			public const int spanCount = 2130771969;
+			
+			// aapt resource value: 0x7f010003
+			public const int stackFromEnd = 2130771971;
+			
 			static Attribute()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
 			}
 			
 			private Attribute()
+			{
+			}
+		}
+		
+		public partial class Dimension
+		{
+			
+			// aapt resource value: 0x7f040000
+			public const int item_touch_helper_max_drag_scroll_per_frame = 2130968576;
+			
+			// aapt resource value: 0x7f040001
+			public const int item_touch_helper_swipe_escape_max_velocity = 2130968577;
+			
+			// aapt resource value: 0x7f040002
+			public const int item_touch_helper_swipe_escape_velocity = 2130968578;
+			
+			static Dimension()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Dimension()
 			{
 			}
 		}
@@ -99,50 +133,65 @@ namespace KoshelnykAndroidTestTask
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f05000b
-			public const int drivingReportBn = 2131034123;
+			// aapt resource value: 0x7f050010
+			public const int CityInputLayout = 2131034128;
+			
+			// aapt resource value: 0x7f050011
+			public const int cityNameET = 2131034129;
 			
 			// aapt resource value: 0x7f05000e
-			public const int forgotYourPassword = 2131034126;
+			public const int drivingReportBn = 2131034126;
 			
-			// aapt resource value: 0x7f050001
-			public const int helpBn = 2131034113;
-			
-			// aapt resource value: 0x7f050008
-			public const int i_c_eBn = 2131034120;
-			
-			// aapt resource value: 0x7f05000c
-			public const int imageView1 = 2131034124;
-			
-			// aapt resource value: 0x7f050007
-			public const int journeySelectionBn = 2131034119;
-			
-			// aapt resource value: 0x7f050000
-			public const int linearLayout1 = 2131034112;
+			// aapt resource value: 0x7f050013
+			public const int forgotYourPassword = 2131034131;
 			
 			// aapt resource value: 0x7f050004
-			public const int linearLayout2 = 2131034116;
+			public const int helpBn = 2131034116;
 			
-			// aapt resource value: 0x7f05000d
-			public const int loginBn = 2131034125;
+			// aapt resource value: 0x7f05000b
+			public const int i_c_eBn = 2131034123;
 			
-			// aapt resource value: 0x7f050006
-			public const int messagingBn = 2131034118;
+			// aapt resource value: 0x7f05000f
+			public const int imageView1 = 2131034127;
 			
-			// aapt resource value: 0x7f050005
-			public const int navigBn = 2131034117;
-			
-			// aapt resource value: 0x7f050009
-			public const int pointOfInterestBn = 2131034121;
-			
-			// aapt resource value: 0x7f050002
-			public const int settingBn = 2131034114;
-			
-			// aapt resource value: 0x7f050003
-			public const int textView1 = 2131034115;
+			// aapt resource value: 0x7f050000
+			public const int item_touch_helper_previous_elevation = 2131034112;
 			
 			// aapt resource value: 0x7f05000a
-			public const int vehicleCheckBn = 2131034122;
+			public const int journeySelectionBn = 2131034122;
+			
+			// aapt resource value: 0x7f050003
+			public const int linearLayout1 = 2131034115;
+			
+			// aapt resource value: 0x7f050007
+			public const int linearLayout2 = 2131034119;
+			
+			// aapt resource value: 0x7f050012
+			public const int loginBn = 2131034130;
+			
+			// aapt resource value: 0x7f050009
+			public const int messagingBn = 2131034121;
+			
+			// aapt resource value: 0x7f050008
+			public const int navigBn = 2131034120;
+			
+			// aapt resource value: 0x7f05000c
+			public const int pointOfInterestBn = 2131034124;
+			
+			// aapt resource value: 0x7f050005
+			public const int settingBn = 2131034117;
+			
+			// aapt resource value: 0x7f050006
+			public const int textView1 = 2131034118;
+			
+			// aapt resource value: 0x7f050001
+			public const int transition_current_scene = 2131034113;
+			
+			// aapt resource value: 0x7f050002
+			public const int transition_scene_layoutid_cache = 2131034114;
+			
+			// aapt resource value: 0x7f05000d
+			public const int vehicleCheckBn = 2131034125;
 			
 			static Id()
 			{
@@ -176,23 +225,23 @@ namespace KoshelnykAndroidTestTask
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f040005
-			public const int AllRightsReserved = 2130968581;
+			// aapt resource value: 0x7f060005
+			public const int AllRightsReserved = 2131099653;
 			
-			// aapt resource value: 0x7f040001
-			public const int ApplicationName = 2130968577;
+			// aapt resource value: 0x7f060001
+			public const int ApplicationName = 2131099649;
 			
-			// aapt resource value: 0x7f040000
-			public const int Hello = 2130968576;
+			// aapt resource value: 0x7f060000
+			public const int Hello = 2131099648;
 			
-			// aapt resource value: 0x7f040004
-			public const int forgotYourPassword = 2130968580;
+			// aapt resource value: 0x7f060004
+			public const int forgotYourPassword = 2131099652;
 			
-			// aapt resource value: 0x7f040002
-			public const int goodMorning = 2130968578;
+			// aapt resource value: 0x7f060002
+			public const int goodMorning = 2131099650;
 			
-			// aapt resource value: 0x7f040003
-			public const int login = 2130968579;
+			// aapt resource value: 0x7f060003
+			public const int login = 2131099651;
 			
 			static String()
 			{
@@ -200,6 +249,45 @@ namespace KoshelnykAndroidTestTask
 			}
 			
 			private String()
+			{
+			}
+		}
+		
+		public partial class Styleable
+		{
+			
+			public static int[] RecyclerView = new int[] {
+					16842948,
+					16842993,
+					2130771968,
+					2130771969,
+					2130771970,
+					2130771971};
+			
+			// aapt resource value: 1
+			public const int RecyclerView_android_descendantFocusability = 1;
+			
+			// aapt resource value: 0
+			public const int RecyclerView_android_orientation = 0;
+			
+			// aapt resource value: 2
+			public const int RecyclerView_layoutManager = 2;
+			
+			// aapt resource value: 4
+			public const int RecyclerView_reverseLayout = 4;
+			
+			// aapt resource value: 3
+			public const int RecyclerView_spanCount = 3;
+			
+			// aapt resource value: 5
+			public const int RecyclerView_stackFromEnd = 5;
+			
+			static Styleable()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Styleable()
 			{
 			}
 		}
