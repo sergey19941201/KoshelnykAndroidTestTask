db050e16fc7567498ddb7076b0d8e2960271c2ad
sergey19941201 <koshelnick1994@mail.ru>
2017-04-04 16:09:07 +0300

    ап

--- KoshelnykAndroidTestTask/RequestClasses/Authentication.cs
+++ KoshelnykAndroidTestTask/RequestClasses/Authentication.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Android.Content;
 using Android.Media;
 using Newtonsoft.Json.Linq;
 using RestSharp;
@@ -14,17 +15,29 @@ namespace KoshelnykAndroidTestTask.RequestClasses
 {
     public class Authentication
     {
-        public void Auth(string login,string password)
+        public void Auth(string login, string password)
         {
+            login = "yaniv@nr.co.il";
+            password = "Aa123456";
             var client = new RestClient("https://networkrail-uk-qa.traffilog.com");
-            var request = new RestRequest("UK/api/User/Login", Method.POST);
+            var request = new RestRequest("/qa/api/User/Login", Method.POST);
             request.AddQueryParameter("username", login);
             request.AddQueryParameter("password", password);
 
             IRestResponse response = client.Execute(request);
             var content = response.Content;
-            //Console.WriteLine("FFFFFFFF" + (int)response.StatusCode);
-            Console.WriteLine(content);
+
+            string[] contentArr = content.Split('"');
+            string token = contentArr[5];
+
+
+
+/*
+            var requestLoginData = new RestRequest("/qa/api/User//UK/api/User/LoginData");
+            requestLoginData.AddQueryParameter("sessionToken", token);
+
+            IRestResponse responseLoginData = client.Execute(requestLoginData);
+            var contentLoginData = responseLoginData.Content;*/
         }
     }
 }
